c2f09cc63bb36146841ae774f9c5ef662e5b134b
neeraj10122004 akula <neerajsaiteja@gmail.com>
2024-05-30 04:46:19 +0000

    new

--- app.py
+++ app.py
@@ -3,29 +3,24 @@ import getpass
 import os
 import sqlite3
 
-gemini_api_key = "GEMINI_API_KEY"
+gemini_api_key = "AIzaSyDYfQmz7AHGbtvY4l5UJGVCa8JJgJrDjaQ"
 genai.configure(api_key = gemini_api_key)
 model = genai.GenerativeModel('gemini-pro')
 
 memory=[]
-database_file_path = './sql_lite_database.db'
-if os.path.exists(database_file_path):
-    os.remove(database_file_path)
-    message = "File 'sql_lite_database.db' found and deleted."
-else:
-    message = "File 'sql_lite_database.db' does not exist."
-conn = sqlite3.connect(database_file_path)
-cursor = conn.cursor()
 
 prompt=input("enter the prompt")
 
 
 def Generate_SQL_Query(question, memory, schema):
+    print("\n\ngenerating query\n\n")
     print(question)
     print(memory)
     data="""you have to generate sql query for the given question using the given this schema: {schema} and this is the memory of the question{memory}
     give only query as answer 
     EXAMPLE: SELECT * FROM DATA
+
+    Assistant:```sql
     """.format(schema=schema,memory=memory)
     model = genai.GenerativeModel('gemini-pro')
     response = model.generate_content(
@@ -37,6 +32,7 @@ def Generate_SQL_Query(question, memory, schema):
 
 
 def Ask_Math_Question_To_LLM(question, memory):
+    print("\n\ndoing math\n\n")
     print(question)
     print(memory)
     data="""you are a calculator you have to do claculations required for the given question and show them and this is the memory of the question {memory}""".format(schema=schema,memory=memory)
@@ -54,30 +50,31 @@ def Agent(question,memory):
     print(memory)
     mem=""
     for i in memory:
-        mem+=i+"\n\n"
-    data="""You are an agent capable of using a variety of TOOLS to answer a data analytics question.
+        mem+=str(i)+"\n\n"
+    data="""<s> [INST]You are an agent capable of using a variety of TOOLS to answer a data analytics question.
     Always use MEMORY to help select the TOOLS to be used.
-    you have access to all the data and data bases you have to soecify the tools needed
- 
+    you have access to all the data and data bases you have to specify the tools needed
+    
     MEMORY
-    {memory}
- 
+    """+mem+"""
+
     TOOLS
     - Generate Final Answer: Use if answer to User's question can be given with MEMORY
     - Calculator: Use this tool to solve mathematical problems.
     - Query_Database: Write an SQL Query to query the Database.
  
     ANSWER FORMAT
-    "json
-    "
+    ```json
+    {
         "tool_name": "Calculator"
-    ""
-
-    User: {prompt}
+    }
+    ```
+    [/INST]
+    User: """+question+"""
  
     Assistant: ```json
-    "
-        "tool_name": """.format(memory=mem,prompt=question)
+    {
+        "tool_name": """
     model = genai.GenerativeModel('gemini-pro')
     response = model.generate_content(
         data,
@@ -113,26 +110,37 @@ def Agent(question,memory):
                             
                 """
         query=Generate_SQL_Query(question, memory, schema)
+        if "```" in query  and "sql" in query :
+            query=query[6:]
+            query=query[:-3]
+        print("\n\n"+query+"\n\n")
+        database_file_path = './sql_lite_database.db'
+        conn = sqlite3.connect(database_file_path)
+        cursor = conn.cursor()
         cursor.execute(query)
         data = cursor.fetchall()
-        memory.append(data)
+        cursor.close()
+        conn.commit()
+        conn.close()
+        memory.append({question:str(data)})
         print(question)
         print(memory)
         Agent(question, memory)
     
     if "Calculator" in tool_selection:
-        Ask_Math_Question_To_LLM(question, memory)
+        memory.append({question:Ask_Math_Question_To_LLM(question, memory)})
         print(question)
         print(memory)
         Agent(question, memory)
 
     if "Generate Final Answer" in tool_selection:
+        print("\n\ngenerating final answer\n\n")
         mem=""
         for i in memory:
-            mem+=i+"\n\n"
+            mem+=str(i)+"\n\n"
         model = genai.GenerativeModel('gemini-pro')
         Final_Output= model.generate_content(
-        [question,mem],
+        question+"give answer base on the memory given"+"memory :"+mem,
         generation_config=genai.types.GenerationConfig(
             temperature=0)
         )
